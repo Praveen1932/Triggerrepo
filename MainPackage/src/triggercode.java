21ce36041a4eb47c8ecff7e5ffb703cd7b15df93
Praveen1932 <praveen.m1@intelizign.com>
2026-01-07 18:17:35 +0530

    Initial commit of Eclipse project

---
+++ MainPackage/src/triggercode.java
@@ -0,0 +1,13 @@
+import com.matrixone.apps.domain.util.MapList;
+
+import matrix.db.Context;
+
+public class triggercode {
+	
+	public MapList demotest(Context context, String[] args) throws Exception
+	{
+		return null;
+		
+	}
+
+}
