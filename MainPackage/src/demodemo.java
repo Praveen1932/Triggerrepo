a23598344c24bf3a7e0cf5652a1a5adc0a5d7104
Praveen1932 <praveen.m1@intelizign.com>
2026-01-08 12:14:24 +0530

    initiate the commit from Eclipse Project

---
+++ MainPackage/src/demodemo.java
@@ -0,0 +1,13 @@
+import com.matrixone.apps.domain.util.MapList;
+
+import matrix.db.Context;
+
+public class demodemo {
+
+	public MapList demo(Context context, String[] args) throws Exception
+	{
+		return null;
+		
+	}
+	
+}
